abd78ed4cdf2f178df105a06126374a43bcbd43e
Edilan Barbosa <edilanbarbosa.contato@gmail.com>
2024-06-12 11:45:09 -0300

    Add files via upload

---
+++ aulas/aula-010/app.js
@@ -0,0 +1,30 @@
+// strings
+console.log("hellow, world");
+
+const email = "edilanbarbosa.contato@gmail.com";
+
+console.log(email);
+
+// concatenação de strings
+const firstName = "Edilan";
+const lastName = "Barbosa";
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// acessando caracteres
+console.log(fullName[0]);
+
+// comprimento de uma string
+console.log(fullName.length);
+
+// métodos de string
+console.log(fullName.toUpperCase());
+
+const result = fullName.toLowerCase();
+
+console.log(result);
+
+const index = email.indexOf("@");
+
+console.log(index);
