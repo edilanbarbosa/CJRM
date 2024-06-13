acc6d648a8449b36999972a9c53da664d6dda912
Edilan Barbosa <edilanbarbosa.contato@gmail.com>
2024-06-13 15:30:01 -0300

    Delete aulas directory

--- aulas/aula-011/app.js
+++
@@ -1,8 +0,0 @@
-// métodos comuns de strings
-const email = "laracroft@rogermelo.com.br";
-
-// const lastIndexOf = email.lastIndexOf('a')
-// const emailSlice = email.slice(4, 9);
-const emailReplace = email.replace("o", "j");
-
-console.log(emailReplace, email);
